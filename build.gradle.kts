14e3c44a27dc26eab41631b12517d2365c5dda16
Sergey Shkurov <sergey.shkurov@traderepublic.com>
2023-12-04 02:10:30 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "ru.serega6531"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
