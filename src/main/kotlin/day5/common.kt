12480363bfcc4f0c252a9ad22067068ca1be48cf
Sergey Shkurov <sergey.shkurov@traderepublic.com>
2023-12-05 15:53:44 +0100

    Day 5 Part 1 (refactor)

---
+++ src/main/kotlin/day5/common.kt
@@ -0,0 +1,57 @@
+package day5
+
+import java.util.NavigableMap
+import java.util.TreeMap
+
+fun solve(maps: List<String>, seeds: Iterable<Long>): Long {
+    val (seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation) = maps
+        .map { parseRelationMap(it) }
+        .map { it.associateByTo(TreeMap()) { relation -> relation.source } }
+
+    val result = seeds.minOf { seed ->
+        val soil = findRelation(seed, seedToSoil)
+        val fertilizer = findRelation(soil, soilToFertilizer)
+        val water = findRelation(fertilizer, fertilizerToWater)
+        val light = findRelation(water, waterToLight)
+        val temperature = findRelation(light, lightToTemperature)
+        val humidity = findRelation(temperature, temperatureToHumidity)
+        val location = findRelation(humidity, humidityToLocation)
+
+        location
+    }
+
+    return result
+}
+
+private fun parseRelationMap(map: String): List<Relation> {
+    return map.lines()
+        .map { it.split(' ') }
+        .map { it.map { s -> s.toLong() } }
+        .map { (destination, source, length) -> Relation(destination, source, length) }
+}
+
+private fun findRelation(from: Long, map: NavigableMap<Long, Relation>): Long {
+    val relation = map.floorEntry(from)?.value
+        ?: return from
+
+    val diff = from - relation.source
+
+    return relation
+        .takeIf { diff < it.length }
+        ?.let { it.destination + diff }
+        ?: from
+}
+
+private data class Relation(
+    val destination: Long,
+    val source: Long,
+    val length: Long
+)
+
+private operator fun <E> List<E>.component6(): E {
+    return get(5)
+}
+
+private operator fun <E> List<E>.component7(): E {
+    return get(6)
+}
--- src/main/kotlin/day5/day5part1.kt
+++ src/main/kotlin/day5/day5part1.kt
@@ -1,8 +1,6 @@
 package day5
 
 import getResourceAsText
-import java.util.NavigableMap
-import java.util.TreeMap
 
 fun main() {
     val input = getResourceAsText("/day5/input.txt")
@@ -12,55 +10,7 @@ fun main() {
     val match = regex.matchEntire(input)!!
 
     val seeds = match.groupValues[1].split(" ").map { it.toLong() }
-    val (seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation) = match.groupValues
-        .drop(2)
-        .map { parseRelationMap(it) }
-        .map { it.associateByTo(TreeMap()) { relation -> relation.source } }
-
-    val result = seeds.minOf { seed ->
-        val soil = findRelation(seed, seedToSoil)
-        val fertilizer = findRelation(soil, soilToFertilizer)
-        val water = findRelation(fertilizer, fertilizerToWater)
-        val light = findRelation(water, waterToLight)
-        val temperature = findRelation(light, lightToTemperature)
-        val humidity = findRelation(temperature, temperatureToHumidity)
-        val location = findRelation(humidity, humidityToLocation)
-
-        location
-    }
+    val result = solve(match.groupValues.drop(2), seeds)
 
     println(result)
-}
-
-private fun parseRelationMap(map: String): List<Relation> {
-    return map.lines()
-        .map { it.split(' ') }
-        .map { it.map { s -> s.toLong() } }
-        .map { (destination, source, length) -> Relation(destination, source, length) }
-}
-
-private fun findRelation(from: Long, map: NavigableMap<Long, Relation>): Long {
-    val relation = map.floorEntry(from)?.value
-        ?: return from
-
-    val diff = from - relation.source
-
-    return relation
-        .takeIf { diff < it.length }
-        ?.let { it.destination + diff }
-        ?: from
-}
-
-private data class Relation(
-    val destination: Long,
-    val source: Long,
-    val length: Long
-)
-
-private operator fun <E> List<E>.component6(): E {
-    return get(5)
-}
-
-private operator fun <E> List<E>.component7(): E {
-    return get(6)
 }
