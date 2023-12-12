613ebd9191b6b9ee2cc592ef4517ff520c276ee1
Sergey Shkurov <sergey.shkurov@traderepublic.com>
2023-12-12 01:14:17 +0100

    Day 11 Part 1

---
+++ src/main/kotlin/day11/day11part1.kt
@@ -0,0 +1,58 @@
+package day11
+
+import getResourceAsText
+import kotlin.math.abs
+
+fun main() {
+    val input = getResourceAsText("/day11/input.txt")
+
+    val lines = input.lines()
+    val maxY = lines.lastIndex
+    val maxX = lines.first().lastIndex
+
+    val galaxiesXY: Array<BooleanArray> = Array(maxY + 1) { y -> BooleanArray(maxX + 1) { x -> lines[y][x] == '#' } }
+    val emptyLines = Array(maxY + 1) { y -> galaxiesXY[y].all { !it } }
+    val emptyColumns = Array(maxX + 1) { x -> galaxiesXY.all { !it[x] } }
+
+    val lineOffsets = calculateOffsets(emptyLines)
+    val columnOffsets = calculateOffsets(emptyColumns)
+
+    val galaxies: List<YX> = (0..maxY).cartesianProduct(0..maxX).filter { (y, x) -> galaxiesXY[y][x] }
+
+    val result = galaxies.permutations { pair1, pair2 ->
+        val lineOffset1 = lineOffsets[pair1.first]
+        val columnOffset1 = columnOffsets[pair1.second]
+
+        val lineOffset2 = lineOffsets[pair2.first]
+        val columnOffset2 = columnOffsets[pair2.second]
+
+        val dy = abs(lineOffset1 - lineOffset2)
+        val dx = abs(columnOffset1 - columnOffset2)
+
+        dy + dx
+    }.sum()
+
+    println(result)
+}
+
+private fun calculateOffsets(presence: Array<Boolean>) = presence.runningFold(0) { acc, empty ->
+    if (empty) {
+        acc + 2
+    } else {
+        acc + 1
+    }
+}
+
+private fun <T, U> Iterable<T>.cartesianProduct(c2: Iterable<U>): List<Pair<T, U>> {
+    return this.flatMap { lhsElem -> c2.map { rhsElem -> lhsElem to rhsElem } }
+}
+
+private fun <T, R> List<T>.permutations(action: (T, T) -> R): List<R> {
+    return this.flatMapIndexed { index, first ->
+        this.subList(index + 1, this.size).map { second ->
+            action(first, second)
+        }
+    }
+}
+
+typealias YX = Pair<Int, Int>
