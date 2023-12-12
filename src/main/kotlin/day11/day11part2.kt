50153fc36e71803e7a0208c8a89e7a9863236340
Sergey Shkurov <sergey.shkurov@traderepublic.com>
2023-12-12 01:35:38 +0100

    Day 11 Part 2

--- src/main/kotlin/day11/day11part2.kt
+++ src/main/kotlin/day11/day11part2.kt
@@ -6,7 +6,7 @@ fun main() {
     val input = getResourceAsText("/day11/input.txt")
 
     val lines = input.lines()
-    val result = solve(lines, expanseSpeed = 2)
+    val result = solve(lines, expanseSpeed = 1000000)
 
     println(result)
 }
